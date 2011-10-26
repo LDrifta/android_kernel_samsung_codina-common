bd856e30a2fc99e8bbb1dc68940254158fea0166
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-26 13:34:18 +0200

    mach-ux500: L2 cache register base name changes in 3.1
    Signed-off-by: Philippe Langlais <philippe.langlais@stericsson.com>

--- arch/arm/mach-ux500/cpu.c
+++ arch/arm/mach-ux500/cpu.c
@@ -38,18 +38,18 @@ static void __iomem *l2x0_base;
 void __init ux500_init_devices(void)
 {
 #ifdef CONFIG_CACHE_L2X0
-        BUG_ON(!l2x0_base);
+	BUG_ON(!l2x0_base);
 
-        /*
-         * Unlock Data and Instruction Lock if locked.  This is done here
-         * instead of in l2x0_init since doing it there appears to cause the
-         * second core boot to occasionaly fail.
-         */
-        if (readl_relaxed(l2x0_base + L2X0_LOCKDOWN_WAY_D) & 0xFF)
-                writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_D);
+	/*
+	 * Unlock Data and Instruction Lock if locked.  This is done here
+	 * instead of in l2x0_init since doing it there appears to cause the
+	 * second core boot to occasionaly fail.
+	 */
+	if (readl_relaxed(l2x0_base + L2X0_LOCKDOWN_WAY_D_BASE) & 0xFF)
+		writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_D_BASE);
 
-        if (readl_relaxed(l2x0_base + L2X0_LOCKDOWN_WAY_I) & 0xFF)
-                writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_I);
+	if (readl_relaxed(l2x0_base + L2X0_LOCKDOWN_WAY_I_BASE) & 0xFF)
+		writel_relaxed(0x0, l2x0_base + L2X0_LOCKDOWN_WAY_I_BASE);
 #endif
 }
 
